7ee090687e956480e7ff9ba7b456dc530b561dad
minanc2032 <98841958+minanc2032@users.noreply.github.com>
2024-04-19 17:31:12 +0300

    Update basic.py

--- backend/py_client/basic.py
+++ backend/py_client/basic.py
@@ -4,11 +4,10 @@ import requests
 #endpoint = "https://httpbin.org/anything"
 endpoint = "http://localhost:8000/api/"
 
-get_response= requests.get(endpoint, params={"product_id":123})
+get_response= requests.get(endpoint, json={"product_id":123})
+#print(get_response.headers)
 #print(get_response.text)
 #print(get_response.status_code)
 
 print(get_response.json())
 #print(get_response.status_code)
-
-
