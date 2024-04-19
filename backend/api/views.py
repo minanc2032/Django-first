df0b7ce9d4123c03a6c22b57baae2ce96788fc3b
minanc2032 <98841958+minanc2032@users.noreply.github.com>
2024-04-19 17:30:46 +0300

    Update views.py

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,18 +1,30 @@
-from django.http import JsonResponse
-import json 
-def api_home(request, *args, **kwargs):
-    #request.body
-    print(request.GET) #URL query parameters 
-    print(request.POST)
-    body = request.body
-    data = {}
-    try:
-        data= json.loads(body)
-    except:
-        pass
-    print(data) 
-    json.dumps(dict(request.headers))
-    data['params'] = dict(request.GET)
-    data['headers'] = dict(request.headers)
-    data['content_type'] = request.content_type
-    return JsonResponse(data)
+import json
+from urllib import request 
+from django.forms.models import model_to_dict
+#from django.http import JsonResponse, HttpResponse
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+from products.models import Product 
+
+#django models
+@api_view(["GET"])
+def api_home(requests, *args, **kwargs):
+    """
+    DRF API VIEW 
+    """
+    if request.method != "POST":
+        return Response({"detail":"Get not allowed"}, status= 405)
+    model_data = Product.objects.all().order_by("?").first()
+    data ={}
+    if model_data:
+        data = model_to_dict(model_data, fields=['id','title','price','sale_price'])
+    return Response(data)
+        
+       
+        #print(data)
+        #data = dict(data)
+        #json_data_str = json.dumps(data)
+    #return HttpResponse(json_data_str, headers={"content-type": "application/json"})
