4e0ba463cf7fb4fce5fe12d4a91cff88e3bf7dee
minanc2032 <98841958+minanc2032@users.noreply.github.com>
2024-04-21 16:30:28 +0300

    Update views.py

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,30 +1,38 @@
 import json
 from urllib import request 
 from django.forms.models import model_to_dict
-#from django.http import JsonResponse, HttpResponse
+#from django.http import JsonResponse
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 
 from products.models import Product 
+from products.serializers import ProductSerializer
 
 #django models
-@api_view(["GET"])
-def api_home(requests, *args, **kwargs):
+@api_view(["POST"])
+def api_home(request, *args, **kwargs):
     """
     DRF API VIEW 
     """
-    if request.method != "POST":
-        return Response({"detail":"Get not allowed"}, status= 405)
-    model_data = Product.objects.all().order_by("?").first()
-    data ={}
-    if model_data:
-        data = model_to_dict(model_data, fields=['id','title','price','sale_price'])
-    return Response(data)
-        
-       
+    #data = request.data 
+    serializer = ProductSerializer(data=request.data )
+    if serializer.is_valid(raise_exception=True):
+        #instance = serializer.save()
+        print(serializer.data)
+        return Response(serializer.data)
+    return Response({"invalid": "not good data"}, status=400)
+
+    #if request.method != "POST":
+        #return Response({"detail":"Get not allowed"}, status= 405)
+   # instance = Product.objects.all().order_by("?").first()
+    #data ={}
+    #if instance:
+        #data = model_to_dict(instance, fields=['id','title','price','sale_price'])
+        #data = ProductSerializer(instance).data   
         #print(data)
         #data = dict(data)
         #json_data_str = json.dumps(data)
     #return HttpResponse(json_data_str, headers={"content-type": "application/json"})
+
