573f86d1d76b7ecfad30770f63d334b9d15e6f32
minanc2032 <98841958+minanc2032@users.noreply.github.com>
2024-04-26 15:58:33 +0300

    Update views.py

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,1 +1,9 @@
+from rest_framework import generics
 
+from .models import Product
+from .serializers import ProductSerializer
+
+
+class ProductDetailAPIView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
