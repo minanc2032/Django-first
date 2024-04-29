1fc9ca6848c4c04981cfdd2b698a6eec5c3d8c15
minanc2032 <98841958+minanc2032@users.noreply.github.com>
2024-04-30 01:41:26 +0300

    Update views.py

--- backend/products/views.py
+++ backend/products/views.py
@@ -3,7 +3,24 @@ from rest_framework import generics
 from .models import Product
 from .serializers import ProductSerializer
 
+class ProductCreateAPIView(generics.CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def perform_create(self, serializer):
+        #serializer.save(user=self.request.user)
+        print(serializer.validated_data)
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('title') or None 
+        if content is None:
+            content = title
+        serializer.save(content=content)
+        # Django signal 
+
+product_create_view = ProductCreateAPIView.as_view()
 
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+product_detail_view = ProductDetailAPIView.as_view()
