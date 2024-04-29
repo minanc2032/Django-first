8557611e734ab9ed058614a55063ed604aaabd2a
minanc2032 <98841958+minanc2032@users.noreply.github.com>
2024-04-30 01:41:45 +0300

    Update urls.py

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:pk>', views.ProductDetailAPIView.as_view())
+    path('', views.product_create_view),
+    path('<int:pk>/', views.product_detail_view)
 ]
