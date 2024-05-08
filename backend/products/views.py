02bd205f5d3c4c75e5c00c771724d06a28939e42
minanc2032 <98841958+minanc2032@users.noreply.github.com>
2024-05-08 23:56:11 +0300

    Update views.py

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, mixins 
+from rest_framework import authentication, generics, mixins, permissions
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
@@ -9,6 +9,8 @@ from .serializers import ProductSerializer
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    authentication_classes = [authentication.SessionAuthentication]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         #serializer.save(user=self.request.user)
@@ -56,15 +58,41 @@ product_destroy_view = ProductDestroyAPIView.as_view()
 
 # product_list_view = ProductListAPIView.as_view()
 
+#class CreateAPIView(mixins.CreateModelMixin, generics.GenericAPIView):
+ #   pass
+
+
 class ProductMixinView(
+    mixins.CreateModelMixin,
     mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
     generics.GenericAPIView
     ):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    lookup_field = "pk"
+
 
     def get(self, request, *args, **kwargs):
+        print(args, kwargs)
+        pk = kwargs.get("pk")
+        if pk is not None:
+            return self.retrieve(request, *args, **kwargs)
         return self.list(request, *args, **kwargs)
+    
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+    
+    def perform_create(self, serializer):
+        #serializer.save(user=self.request.user)
+        #print(serializer.validated_data)
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content') or None 
+        if content is None:
+            content = "this is a single view doing cool stuff"
+        serializer.save(content=content)
+
+        # Django signal 
     #def post() #HTTP--> post 
 
 product_mixin_view = ProductMixinView.as_view()
