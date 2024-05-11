a0b620dc5adca24c3ecb497b45050290f6d2b5c3
minanc2032 <98841958+minanc2032@users.noreply.github.com>
2024-05-11 17:43:35 +0300

    Update settings.py

--- backend/cfehome>/settings.py
+++ backend/cfehome>/settings.py
@@ -38,9 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'api',
     'products'
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
